298cd4187bcefb8a80d5f0980cfb0094dc8351b8
SarCasm <sarcasm008@gmail.com>
2017-09-02 02:58:55 +0300

    Recompiled assets

---
+++ public/assets/pickup_point_preview-df702b8953b8450d768b07c19e12cf6228a1581b1613b9b2114c354b25f6e8f8.js
@@ -0,0 +1,38 @@
+var map = {};
+var marker;
+var activePoint;
+function initMap() {
+  var mapAnchor = document.getElementById("google-map");
+  var lat = $(mapAnchor).data('lat');
+  var lng = $(mapAnchor).data('lng');
+  var myOptions = {
+    zoom: 18,
+    center: new google.maps.LatLng(lat, lng),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    scrollwheel: false
+  };
+  map = new google.maps.Map(mapAnchor, myOptions);
+  marker = new google.maps.Marker({
+    position: new google.maps.LatLng(lat, lng),
+    map: map
+  });
+}
+
+function selectPoint(point) {
+  if (activePoint)
+    activePoint.removeClass('active')
+
+  activePoint = $(point)
+  $(point).addClass('active')
+  var lat = $(point).data('lat')
+  var lng = $(point).data('lng')
+  marker.setPosition(new google.maps.LatLng(lat, lng));
+  map.setCenter(new google.maps.LatLng(lat, lng));
+}
+
+$(document).ready(function() {
+  $('.js-pickup-point').on('click', function(e) {
+    e.preventDefault()
+    selectPoint(this)
+  })
+});
