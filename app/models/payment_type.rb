2961616e4f5e680e9bfee48b5090562ba22f99c3
SarCasm <sarcasm008@gmail.com>
2017-09-18 20:18:40 +0300

    Make PaymentType correct List View

--- app/models/payment_type.rb
+++ app/models/payment_type.rb
@@ -16,12 +16,15 @@ class PaymentType < ApplicationRecord
   rails_admin do
     navigation_label 'Settings'
     weight 999
+
     list do
+      field :id
       field :payment_type
       field :tour_hotels do
         eager_load true
       end
     end
+
     edit do
       field :payment_type
     end
