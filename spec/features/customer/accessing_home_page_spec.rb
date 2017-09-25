2791c9bf772b1724e0ef58cdf1b968a8478919d7
SarCasm <sarcasm008@gmail.com>
2017-09-25 20:16:15 +0300

    Fix typo

--- spec/features/customer/accessing_home_page_spec.rb
+++ spec/features/customer/accessing_home_page_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "Accessing Home Page", :type => :feature do
-  it "loads home oage" do
+  it "loads home page" do
     visit '/'
     expect(page).to have_content 'Tour Platform'
   end
