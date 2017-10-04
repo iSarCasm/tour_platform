68523736b01ee1d3e463a643f49620c7f36c06fb
SarCasm <sarcasm008@gmail.com>
2017-10-04 21:36:05 +0300

    Spec for managing photos

---
+++ spec/features/admin/managing_photos_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe 'Managing Photos', type: :feature do
+  include Devise::Test::IntegrationHelpers
+
+  context 'when logged in as Admin' do
+    before do
+      sign_in create(:superadmin)
+    end
+
+    context 'with some Photos' do
+      before do
+        @photo = create :hotel_photo
+        create :hotel_photo
+        create :hotel_photo
+      end
+
+      it 'can List all Photos' do
+        visit '/admin/photo'
+        expect(page).to have_content '3 photos'
+      end
+
+      it 'can Delete an Photo' do
+        visit "/admin/photo/#{@photo.id}/delete"
+        page.find(:css, '.btn-danger').click
+        visit '/admin/photo'
+        expect(page).to have_content '2 photos'
+      end
+
+      it 'can View the Photo' do
+        visit "/admin/photo/#{@photo.id}"
+        expect(page).to have_content 'Photo'
+      end
+
+      it 'can Edit the Photo' do
+        visit "/admin/photo/#{@photo.id}/edit"
+        expect(page).to have_content 'Photo'
+      end
+    end
+  end
+end
