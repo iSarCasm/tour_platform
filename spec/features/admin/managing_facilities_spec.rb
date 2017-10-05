10c59218182d21f5be946a72d1ab3465b06fa712
SarCasm <sarcasm008@gmail.com>
2017-10-05 19:23:28 +0300

    Spec for managing facilities

---
+++ spec/features/admin/managing_facilities_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+describe 'Managing Facilities', type: :feature do
+  include Devise::Test::IntegrationHelpers
+
+  context 'when logged in as Admin' do
+    before do
+      sign_in create(:superadmin)
+    end
+
+    context 'with some Facilities' do
+      before do
+        @facility = create :facility, facility: 'Crazy one'
+        create :facility, facility: 'Some basis'
+        create :facility, facility: 'another one'
+      end
+
+      it 'can List all Facilities' do
+        visit '/admin/facility'
+
+        expect(page).to have_content 'Crazy one'
+        expect(page).to have_content 'Some basis'
+        expect(page).to have_content 'another one'
+        expect(page).to have_content '3 facilities'
+      end
+
+      it 'can Delete an Facility' do
+        visit "/admin/facility/#{@facility.id}/delete"
+        page.find(:css, '.btn-danger').click
+        visit '/admin/facility'
+
+        expect(page).not_to have_content 'Crazy one'
+        expect(page).to have_content 'Some basis'
+        expect(page).to have_content 'another one'
+        expect(page).to have_content '2 facilities'
+      end
+
+      it 'can View the Facility' do
+        visit "/admin/facility/#{@facility.id}"
+        expect(page).to have_content 'Facility'
+        expect(page).to have_content 'Crazy one'
+      end
+
+      it 'can Edit the Facility' do
+        visit "/admin/facility/#{@facility.id}/edit"
+        expect(page).to have_content 'Facility'
+
+        fill_in 'facility[facility]', with: 'Wut face'
+        page.find(:css, '.btn-primary').click
+
+        visit '/admin/facility'
+        expect(page).not_to have_content 'Crazy one'
+        expect(page).to have_content 'Wut face'
+      end
+    end
+  end
+end
