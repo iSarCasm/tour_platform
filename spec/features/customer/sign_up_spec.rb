083175ef39a225cf3cd1d121934c27a2bb89d265
SarCasm <sarcasm008@gmail.com>
2017-10-06 17:03:55 +0300

    Sign up spec for duplicate emails

--- spec/features/customer/sign_up_spec.rb
+++ spec/features/customer/sign_up_spec.rb
@@ -14,4 +14,25 @@ describe 'Sign up', type: :feature do
 
     expect(page).to have_content 'Welcome! You have signed up successfully.'
   end
+
+  context 'when email is taken' do
+    before do
+      create :user, email: 'jackdaniels@gmail.com'
+    end
+
+    it 'denies from signing up and notifies of email duplicate' do
+      visit '/users/sign_up'
+
+      expect(page).to have_content 'Sign Up'
+
+      fill_in 'Name', with: 'Jack Daniels'
+      fill_in 'Email', with: 'jackdaniels@gmail.com'
+      fill_in 'Password', with: 'secure_password'
+      fill_in 'Password confirmation', with: 'secure_password'
+      click_button 'Sign up'
+
+      expect(page).to have_content 'Email has already been taken'
+      expect(page).not_to have_content 'Welcome! You have signed up successfully.'
+    end
+  end
 end
