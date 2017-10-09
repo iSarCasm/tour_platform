1ff7a3ece63d68f87fca4baefba810b162feb246
SarCasm <sarcasm008@gmail.com>
2017-10-09 23:50:10 +0300

    Default hotel rooms for Hotel

--- spec/factories/hotel_room.rb
+++ spec/factories/hotel_room.rb
@@ -1,7 +1,16 @@
 FactoryGirl.define do
   factory :hotel_room do
     amount      { Faker::Number.between(1, 400) }
+    adult       100
+    adult_supp  10
+    child       100
+    child_supp  10
+    senior      100
+    senior_supp 10
+    infant      100
+    infant_supp 10
     room_type
     tour_hotel
+    hotel
   end
 end
