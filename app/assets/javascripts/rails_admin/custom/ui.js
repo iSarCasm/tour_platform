c4fea097d6b1e54de6d76a96385ba6c4a4f012e1
SarCasm <sarcasm008@gmail.com>
2017-11-15 19:45:58 +0200

    Add js workaround to get 'Add' button working

--- app/assets/javascripts/rails_admin/custom/ui.js
+++ app/assets/javascripts/rails_admin/custom/ui.js
@@ -5,20 +5,7 @@
 //= require jquery.livestamp
 //= require jquery.timer
 //= require jquery.notificationcenter
-
-$(document).ready(function() {
-  // Table view ADD button hack
-  var oldNestedFormEvents = window.nestedFormEvents.insertFields
-  window.nestedFormEvents.insertFields = function (content, assoc, link) {
-    var insertable = $(link).parent().siblings('#tab_logic').find('.js-insertable');
-    if (insertable.length > 0) {
-      insertable.first().append(content);
-      return insertable.first().children().last();
-    } else {
-      return oldNestedFormEvents(content, assoc, link);
-    }
-  }
-});
+//= require table-field
 
 // Horizontaly scrolling table
 $(document).on('rails_admin.dom_ready', function(){
