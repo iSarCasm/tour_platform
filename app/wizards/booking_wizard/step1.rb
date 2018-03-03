d22a05fb4cae1652f27c65fb224066ce817c1ef5
SarCasm <sarcasm008@gmail.com>
2018-03-03 19:47:24 +0200

    Add step1 specs

--- app/wizards/booking_wizard/step1.rb
+++ app/wizards/booking_wizard/step1.rb
@@ -13,12 +13,25 @@ class BookingWizard::Step1 < BookingWizard::Base
 
   def save
     tour_booking = TourBooking.new(tour_booking_params)
-    @attributes = { tour_booking: tour_booking } 
+    hotel_bookings = create_hotel_bookings(params: hotel_booking_params, tour_booking: tour_booking)
+    @attributes = { tour_booking: tour_booking, hotel_bookings: hotel_bookings } 
   end
 
   private
 
   def tour_booking_params
     params.require(:tour_booking).permit(:active_tour_id, :adult, :child, :infant, :senior)
+  end
+
+  def hotel_booking_params
+    params[:hotel_booking].permit!.to_h
+  end
+
+  def create_hotel_bookings(params:, tour_booking:)
+    params.each_with_object([]) do |(hotel_room_id, amount), bookings|
+      amount.to_i.times do
+        bookings << HotelBooking.new(tour_booking: tour_booking, hotel_room_id: hotel_room_id)
+      end
+    end
   end
 end
--- spec/wizards/booking_wizard/step1_spec.rb
+++ spec/wizards/booking_wizard/step1_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe BookingWizard::Step1 do
   let(:subject) { BookingWizard::Step1.new(params: params, session: session) }
   let(:session) { nil }
-  let(:params) { { active_tour_slug: 'abc' } }
+  let(:params) { ActionController::Parameters.new({ active_tour_slug: 'abc' }) }
 
   describe '#view' do
     it 'returns path to view file' do
@@ -22,21 +22,75 @@ describe BookingWizard::Step1 do
   end
 
   describe '#step_initalize' do
-    it 'assigns #booking'
+    let!(:tour) { create :tour }
+    let!(:active_tour) { create :active_tour, slug: 'abc', tour: tour }
+
+    it 'assigns #booking' do
+      expect(subject.booking).to be_a TourBooking
+    end
 
     describe 'assigns #active_tour' do
-      it 'finds by slug'
+      it 'finds by slug' do
+        expect(subject.active_tour).to eq active_tour
+      end
 
       it 'configures for form'
     end
 
-    it 'assigns #tour'
+    it 'assigns #tour' do
+      expect(subject.tour).to eq active_tour.tour
+    end
   end
 
   describe '#save' do
-    it 'adds tour_booking to #attributes hash'
+    let!(:active_tour) { create :active_tour }
+    let(:params) do 
+      ActionController::Parameters.new({
+        tour_booking: {
+          active_tour_id: active_tour.id,
+          adult: 2,
+          child: 1,
+          infant: 3,
+          senior: 4
+        },
+        hotel_booking: {
+          13 => 2,
+          9 => 1,
+          4 => 3
+        }
+      })
+    end
 
-    it 'adds hotel_bookings to #attributes hash'
+    describe 'adds tour_booking to #attributes hash' do
+      it 'initializes from params' do
+        stub_step_initialize
+        
+        subject.save
+  
+        expect(subject.attributes[:tour_booking].active_tour_id).to eq active_tour.id
+        expect(subject.attributes[:tour_booking].adult).to eq 2
+        expect(subject.attributes[:tour_booking].child).to eq 1
+        expect(subject.attributes[:tour_booking].infant).to eq 3
+        expect(subject.attributes[:tour_booking].senior).to eq 4
+      end
+    end
+
+    describe 'adds hotel_bookings to #attributes hash' do
+      xit 'initializes N bookings for each hotel' do
+        stub_step_initialize
+
+        subject.save
+
+        expect(subject.attributes[:hotel_bookings]).to match_array([
+          HotelBooking.new(tour_booking: subject.attributes[:tour_booking], hotel_room_id: 13),
+          HotelBooking.new(tour_booking: subject.attributes[:tour_booking], hotel_room_id: 13),
+          HotelBooking.new(tour_booking: subject.attributes[:tour_booking], hotel_room_id: 9),
+          HotelBooking.new(tour_booking: subject.attributes[:tour_booking], hotel_room_id: 4),
+          HotelBooking.new(tour_booking: subject.attributes[:tour_booking], hotel_room_id: 4),
+          HotelBooking.new(tour_booking: subject.attributes[:tour_booking], hotel_room_id: 4)
+        ])
+      end
+    end
   end
 
   def stub_step_initialize
